3d4dedf0ce626e30c66ffbc484d59d734238e130
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2012-06-27 14:43:10 +0200

    + cache

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var pageshot = require('./lib/pageshot')
 var app = require('express').createServer()
 
 app.get('/*', function(req, res){
-  pageshot(req.params[0], function(err, data){
+  pageshot(req.params[0], {}, function(err, data){
     if (err) throw err
     res.contentType('image/jpeg')
     res.send(new Buffer(data, 'binary'))
