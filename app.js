9a02abf803ff9a3d9bf2176e941a8a8f7d0d1dfb
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2012-08-17 16:37:20 +0200

    + resize and crop thumbnail

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var pageshot = require('./lib/pageshot')
 var app = require('express').createServer()
 
-app.get('/*', function(req, res){
-  pageshot(req.params[0], {}, function(err, data){
+app.get('/', function(req, res){
+  var url = req.query.url
+  delete req.query.url
+
+  pageshot(url, req.query, function(err, data){
     if (err) throw err
     res.contentType('image/jpeg')
     res.send(new Buffer(data, 'binary'))
