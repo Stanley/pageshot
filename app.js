e9f76ad0ce52dd8bffa102f0ded3112f7087195e
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2012-11-06 10:08:50 +0000

    + error handling

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var pageshot = require('./lib/pageshot')
-var app = require('express').createServer()
+var app = require('express')()
 
 app.get('/', function(req, res){
   var url = req.query.url
   delete req.query.url
 
   pageshot(url, req.query, function(err, data){
-    if (err) throw err
-    res.contentType('image/jpeg')
-    res.send(new Buffer(data, 'binary'))
+    if (err) {
+      res.send(500, err)
+    } else {
+      res.contentType('image/jpeg')
+      res.send(new Buffer(data, 'binary'))
+    }
   })
 });
 
